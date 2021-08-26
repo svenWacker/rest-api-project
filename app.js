7b14451873a0ec14e75185e65667d4932fd581b7
svenWacker <s.u.wacker@outlook.de>
2021-08-26 10:10:02 +0200

    connection established

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+//  Development mode info
+app.use(morgan("dev"));
+// to process the json data
+app.use(express.json());
+
+// mongoDB
+const mongoose = require("mongoose");
+const DB_URL = process.env.DB_URL;
+mongoose
+  .connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(console.log("userDB is connected"))
+  .catch((error) => {
+    console.log(`There was a problem ${error.message}`);
+  });
+// http:localhost:5000/user
+const employees = require("./router/user");
+// app.use("/user", user);
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to our user-app");
+});
+
+module.exports = app;
